efaaa224b61a7b81f1af936925b18302edc0ec64
Ian_Do_041 <130380548+IanF13@users.noreply.github.com>
2024-09-23 10:59:47 -0300

    Add files via upload

---
+++ Troops/Pawn/pawn.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+class_name BaseCharacter
+
+@export_category("Variables")
+@export var _move_speed: float = 128.0
+
+func _physics_process(_delta: float) -> void:
+		pass
+		
+		
+	func _physics_process(_delta: float) -> void:
+		var _direction: Vector2 = Input.get_vector(
+			"move left", "move_right", "move_up", "move_down"
+		)
+		velocity = _direction * _move_speed
+		print (_direction.lenght())
+		
+		
